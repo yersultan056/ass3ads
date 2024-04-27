f8df8baefca12406a6ac3a034e4e03e3b7a0ad66
yersik <230129@astanait.edu.kz>
2024-04-27 11:56:42 +0600

    1

--- src/task5.java
+++ src/task5.java
@@ -2,44 +2,33 @@ import java.util.*;
 
 public class task5 {
     public static int countStudents(int[] students, int[] samsas) {
-        Queue<Integer> queue = new LinkedList<>();
-        for (int student : students) {
-            queue.add(student);
+        Stack<Integer> stack = new Stack<>();
+        for (int samsa : samsas) {
+            stack.push(samsa);
         }
         int count = 0;
-        for (int samsa : samsas) {
-            int size = queue.size();
-            while (!queue.isEmpty() && queue.peek() != samsa) {
-                queue.add(queue.remove());
+        for (int student : students) {
+            if (!stack.isEmpty() && stack.peek() == student) {
+                stack.pop();
+            } else {
                 count++;
-                if (count == size) {
-                    return queue.size();
-                }
             }
-            if (!queue.isEmpty()) {
-                queue.remove();
-            }
-            count = 0;
         }
-        return queue.size();
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
         int n = scanner.nextInt();
         int[] students = new int[n];
         int[] samsas = new int[n];
-        System.out.println();
         for (int i = 0; i < n; i++) {
             students[i] = scanner.nextInt();
         }
-        System.out.println();
         for (int i = 0; i < n; i++) {
             samsas[i] = scanner.nextInt();
         }
-        int output = countStudents(students, samsas);
-        System.out.println(Arrays.toString(students) + Arrays.toString(samsas));
-        System.out.println(output);
+        int unableToEat = countStudents(students, samsas);
+        System.out.println(unableToEat);
     }
 }
